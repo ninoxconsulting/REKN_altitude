c60bcc8ad2d49a42327c6c148d0dc04b6407ab9d
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2025-04-03 10:28:05 -0700

    update cams edits to loop through birds

--- 01_loaddata.R
+++ 01_loaddata.R
@@ -12,76 +12,91 @@ library(lubridate)
 ## DATA PRE-PROCESSING ##
 # Read CSV Data
 
-# TODO: add a purrr::map to be able to loop through many files/birds, 
-# need to add a value to distinguish the tag id.
-
 ### List all bird files in the inputs folder
-bird_files <- list.files(path("01_inputs/bird_data")) # Read in a list of all bird CSV data
+bird_files <- list.files(path("01_inputs/bird_data"), recursive = TRUE, pattern = ".csv") # Read in a list of all bird CSV data
+
+bird_files <- bird_files[1:60]
 
 ### Loop through each existing bird CSV file and perform the analyses
 birds_processed <- purrr::map(bird_files, function(b) {
+  # testing
+  # b <- bird_files[1]
 
-## 0) Data Pre-Processing
-# Read in CSV Data
-birds <- read_csv(path("01_inputs/bird_data", b), name_repair = "unique", locale = locale(encoding = "latin1"))
+  bname <- gsub(".*/(.*)", "\\1", b)
 
-# Convert to SF Object
-birds_sf <- st_as_sf(birds, coords = c("Longitude", "Latitude"), crs = 4326, remove = FALSE)
+  cli::cli_alert_info("processing {bname}")
 
-# Filter Out Bad Data and Sort by Date Ascending
-birds_sf <- birds_sf %>% filter(CRC != "Fail")
+  ## 0) Data Pre-Processing
+  # Read in CSV Data
+  birds <- read_csv(path("01_inputs/bird_data", b), name_repair = "unique", locale = locale(encoding = "latin1"))
 
-birds_sf <- birds_sf |>
-  dplyr::mutate(datetime = dmy_hms(paste(Date, Time, sep = " ")))
+  # Filter Out Bad Data and Sort by Date Ascending
+  birds <- birds %>% filter(CRC != "Fail")
 
-birds_sf <- birds_sf %>% arrange(datetime)
+  # Convert to SF Object
+  birds_sf <- st_as_sf(birds, coords = c("Longitude", "Latitude"), crs = 4326, remove = FALSE)
 
-birds_sf <- birds_sf |>
-  mutate(
-    location.long_prior = lag(Longitude, 1L),
-    location.lat_prior = lag(Latitude, 1L)
-  )
+  birds_sf <- birds_sf |>
+    dplyr::mutate(datetime = dmy_hms(paste(Date, Time, sep = " ")))
 
+  birds_sf <- birds_sf %>% arrange(datetime)
 
-# Calculate Distance Between Sequential Ping Locations
-birds_dist <- st_distance(birds_sf) # Creates a distance matrix for every point's distance to every other point in the dataset (in metres)
+  # add a check to remove points that are obviously wrong but not flagged as fail.
+  birds_sf$year <- year(birds_sf$datetime) # add a year column to the dataset
+  birds_sf <- birds_sf |>
+    filter(year < 2025) |>
+    select(-year)
 
-dist_seq <- c() # creates an empty vector to store sequential distances
+  birds_sf <- birds_sf |>
+    mutate(
+      location.long_prior = lag(Longitude, 1L),
+      location.lat_prior = lag(Latitude, 1L),
+      filename = bname
+    )
 
-for (i in 1:nrow(birds_sf)) { # loops through every GPS ping in the dataset
-  if (i == 1) { # if it's the very first point in the dataset, it will calculate the very first distance calculation, which will be zero
-    dist_seq[i] <- birds_dist[i, i]
-  } else { # any other element will be calculating the distance between the current point and the previous ping location
-    dist_seq[i] <- birds_dist[i, i - 1]
+  # Calculate Distance Between Sequential Ping Locations
+  birds_dist <- st_distance(birds_sf) # Creates a distance matrix for every point's distance to every other point in the dataset (in metres)
+
+  dist_seq <- c() # creates an empty vector to store sequential distances
+
+  for (i in 1:nrow(birds_sf)) { # loops through every GPS ping in the dataset
+    if (i == 1) { # if it's the very first point in the dataset, it will calculate the very first distance calculation, which will be zero
+      dist_seq[i] <- birds_dist[i, i]
+    } else { # any other element will be calculating the distance between the current point and the previous ping location
+      dist_seq[i] <- birds_dist[i, i - 1]
+    }
   }
-}
 
-dist_km <- dist_seq / 1000 # converts the distances from metres to kilometres
+  dist_km <- dist_seq / 1000 # converts the distances from metres to kilometres
 
-birds_sf$dist_km <- dist_km # add the distance column to the table
-
+  birds_sf$dist_km <- dist_km # add the distance column to the table
 
 
-## 2) calculate the time between ping locations
+  ## 2) calculate the time between ping locations
 
-birds_sf$timediff_hrs <- as.numeric(difftime(birds_sf$datetime, lag(birds_sf$datetime), units = "hours"))
+  birds_sf$timediff_hrs <- as.numeric(difftime(birds_sf$datetime, lag(birds_sf$datetime), units = "hours"))
 
-## 3) calculate the bearing and speed of travel
+  ## 3) calculate the bearing and speed of travel
 
-birds_sf <- birds_sf |>
-  rowwise() |>
-  dplyr::mutate(
-    bearing = bearing(c(location.long_prior, location.lat_prior), c(Longitude, Latitude)),
-    speed_kmh = round((dist_km / timediff_hrs), 1)
-  )
+  birds_sf <- birds_sf |>
+    rowwise() |>
+    dplyr::mutate(
+      bearing = bearing(c(location.long_prior, location.lat_prior), c(Longitude, Latitude)),
+      speed_kmh = round((dist_km / timediff_hrs), 1)
+    )
 
-# Convert bearings to azimuths
-for(i in 2:nrow(birds_sf)){
-  if(birds_sf[i,]$bearing < 0){
-    birds_sf[i,]$bearing <- birds_sf[i,]$bearing + 360 # adds the difference of 360 to any negative values to get the azimuth
+  # Convert bearings to azimuths
+  for (i in 2:nrow(birds_sf)) {
+    if (birds_sf[i, ]$bearing < 0) {
+      birds_sf[i, ]$bearing <- birds_sf[i, ]$bearing + 360 # adds the difference of 360 to any negative values to get the azimuth
+    }
   }
-}
 
+  return(birds_sf)
+}) |> bind_rows()
+
+
+birds_sf <- birds_processed
 
 ## 4) Calculate and Convert Geoid Heights
 
@@ -90,7 +105,7 @@ EGM2008_1 <- rast(path("01_inputs", "us_nga_egm2008_1.tif")) # The Earth Gravita
 birds_vect <- vect(birds_sf) # converting the birds dataset to a SpatVector
 birds_vect <- project(birds_vect, "EPSG:4979") # reprojecting birds dataset to the same CRS as the geoid model
 
-geoid_height <- terra::extract(EGM2008_1, birds_vect)  # tested these calculated geoid values against the geoid height calculator and the values matched
+geoid_height <- terra::extract(EGM2008_1, birds_vect) # tested these calculated geoid values against the geoid height calculator and the values matched
 geoid_height <- geoid_height$geoid_undulation # the extract function creates a data frame so this function just gets the geoid values as a vector
 
 
@@ -115,18 +130,15 @@ birds_sf$offshore <- !birds_sf$onshore & !nearshore # any pings not meeting onsh
 
 shore_status <- c()
 
-for(i in 1:nrow(birds_sf)){ # loop through bird data and assign onshore/offshore/nearshore based on shore comparison columns
-  if(birds_sf[i,]$onshore == TRUE && birds_sf[i,]$offshore == FALSE && birds_sf[i,]$nearshore == FALSE){
-    shore_status[i] <- 'Onshore'
-  }
-  else if(birds_sf[i,]$onshore == FALSE && birds_sf[i,]$offshore == TRUE && birds_sf[i,]$nearshore == FALSE){
-    shore_status[i] <- 'Offshore'
-  }
-  else if(birds_sf[i,]$onshore == FALSE && birds_sf[i,]$offshore == FALSE && birds_sf[i,]$nearshore == TRUE){
-    shore_status[i] <- 'Nearshore'
-  }
-  else{
-    shore_status[i] <- 'ERROR'
+for (i in 1:nrow(birds_sf)) { # loop through bird data and assign onshore/offshore/nearshore based on shore comparison columns
+  if (birds_sf[i, ]$onshore == TRUE && birds_sf[i, ]$offshore == FALSE && birds_sf[i, ]$nearshore == FALSE) {
+    shore_status[i] <- "Onshore"
+  } else if (birds_sf[i, ]$onshore == FALSE && birds_sf[i, ]$offshore == TRUE && birds_sf[i, ]$nearshore == FALSE) {
+    shore_status[i] <- "Offshore"
+  } else if (birds_sf[i, ]$onshore == FALSE && birds_sf[i, ]$offshore == FALSE && birds_sf[i, ]$nearshore == TRUE) {
+    shore_status[i] <- "Nearshore"
+  } else {
+    shore_status[i] <- "ERROR"
   }
 }
 
@@ -135,8 +147,6 @@ birds_sf$shore_status <- shore_status # add the shore status column to the bird 
 birds_sf <- subset(birds_sf, select = -c(onshore, offshore, nearshore)) # drop the shore comparison columns
 
 ## Returns the bird_sf as the output for each iteration of the loop
-return(birds_sf)
-})
 
 ## Create output folder for storing data if folder does not exist
 if (!dir.exists(path("02_outputs"))) {
@@ -147,7 +157,4 @@ if (!dir.exists(path("02_outputs/birds_raw_proc"))) {
   dir.create(path("02_outputs/birds_raw_proc"))
 }
 
-## Save each processed bird dataset to the outputs folder for further processing for wind data
-for(f in 1:length(birds_processed)){
-  st_write(birds_processed[[f]], path("02_outputs/birds_raw_proc", paste("birds_raw_", f, ".gpkg", sep = "")), driver = "GPKG")
-}
+st_write(birds_sf, path("02_outputs/birds_raw_proc", "birds_raw.gpkg"), driver = "GPKG")
